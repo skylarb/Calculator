94f75a8e7676f43b8d7f57b8f4666b340297eda7
skylarb <skylar_baley@yahoo.com>
2020-09-25 09:52:15 -0500

    Final Failure

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -9,11 +9,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
 
     private Button one, two, three, four, five, six, seven, eight, nine, zero, add, sub, multi,
-            divi, deci, equals, negative, back;
+            divi, deci, equals, negative, back, clear;
     private TextView formula, result;
+
+    boolean ADD, SUB, MULTI, DIVI;
+
+    private float num1 = Float.NaN;
+    private float num2;
+
 
 
     @Override
@@ -25,63 +31,166 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                formula.setText(formula.getText().toString() + "1");
+                formula.setText(formula.getText() + "1");
             }
         });
         two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                formula.setText(formula.getText().toString() + "2");
+                formula.setText(formula.getText() + "2");
             }
         });
         three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                formula.setText(formula.getText().toString() + "3");
+                formula.setText(formula.getText() + "3");
             }
         });
         four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                formula.setText(formula.getText().toString() + "4");
+                formula.setText(formula.getText() + "4");
             }
         });
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                formula.setText(formula.getText().toString() + "5");
+                formula.setText(formula.getText() + "5");
             }
         });
         six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                formula.setText(formula.getText().toString() + "6");
+                formula.setText(formula.getText() + "6");
             }
         });
         seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                formula.setText(formula.getText().toString() + "7");
+                formula.setText(formula.getText() + "7");
             }
         });
         eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                formula.setText(formula.getText().toString() + "8");
+                formula.setText(formula.getText() + "8");
             }
         });
         nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                formula.setText(formula.getText().toString() + "9");
+                formula.setText(formula.getText() + "9");
             }
         });
         zero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                formula.setText(formula.getText().toString() + "0");
+                formula.setText(formula.getText() + "0");
             }
         });
+        deci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                formula.setText(formula.getText() + ".");
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (formula.getText().length() != 0){
+                    num1 = Float.parseFloat(formula.getText() + "");
+                    ADD = true;
+                    formula.setText(null);
+                }
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (formula.getText().length() != 0){
+                    num1 = Float.parseFloat(formula.getText() + "");
+                    SUB = true;
+                    formula.setText(null);
+                }
+            }
+        });
+        multi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (formula.getText().length() != 0){
+                    num1 = Float.parseFloat(formula.getText() + "");
+                    MULTI = true;
+                    formula.setText(null);
+                }
+            }
+        });
+        divi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (formula.getText().length() != 0){
+                    num1 = Float.parseFloat(formula.getText() + "");
+                    DIVI = true;
+                    formula.setText(null);
+                }
+            }
+        });
+        negative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                negate();
+            }
+        });
+        equals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ADD || SUB || MULTI || DIVI){
+                    num2 = Float.parseFloat(formula.getText() + "");
+                }
+                if (ADD) {
+                    formula.setText(num1 + num2 + "");
+                    ADD = false;
+                }
+                if (SUB) {
+                    formula.setText(num1 - num2 + "");
+                    SUB = false;
+                }
+                if (MULTI) {
+                    formula.setText(num1 * num2 + "");
+                    SUB = false;
+                }
+                if (DIVI) {
+                    formula.setText(num1 / num2 + "");
+                    DIVI = false;
+                }
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(formula.getText().length() > 0){
+                    CharSequence input = formula.getText().toString();
+                    formula.setText(input.subSequence(0, input.length() - 1));
+                } else{
+                    num1 = Float.NaN;
+                    num2 = Float.NaN;
+                    formula.setText(null);
+                    result.setText(null);
+                }
+            }
+
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num1 = Float.NaN;
+                num2 = Float.NaN;
+                formula.setText(null);
+                result.setText(null);
+            }
+
+        });
+
+
 
     }
 
@@ -108,13 +217,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         negative = (Button)findViewById(R.id.buttonNeg);
         equals = (Button)findViewById(R.id.buttonEquals);
         back = (Button)findViewById(R.id.buttonBack);
+        clear = (Button)findViewById(R.id.buttonClear);
         formula = (TextView)findViewById(R.id.tvFormula);
         result = (TextView)findViewById(R.id.tvResult);
     }
-
-    @Override
-    public void onClick(View view) {
-        String myButtonTag = String.valueOf(view.getId());
-        Log.d("MainActivity", "Button Clicked" + myButtonTag + " Clicked");
+    private void negate(){
+        num1 *= (-1);
     }
 }
